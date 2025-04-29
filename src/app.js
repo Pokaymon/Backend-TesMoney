e65f5505f3add7eb6be8a33705a11aaf85e62142
Ubuntu <pokaymon.mno@gmail.com>
2025-04-29 14:35:17 +0000

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import categoriasRoutes from "./routes/categorias.routes.js";
-import clientesRoutes from "./routes/clientes.routes.js";
-import empleadosRoutes from "./routes/empleados.routes.js";
 import apiRoutes from "./routes/api.routes.js";
 import pocketRoutes from "./routes/pocketRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
@@ -15,8 +13,6 @@ app.use(express.json());
 app.set("port", 5000)
 
 app.use("/api/categorias",categoriasRoutes);
-app.use("/api/clientes", clientesRoutes);
-app.use("/api/empleados", empleadosRoutes);
 app.use("/api", apiRoutes);
 app.use("/pocket", pocketRoutes);
 app.use("/transactions", transactionRoutes);
